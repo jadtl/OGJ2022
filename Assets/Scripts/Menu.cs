50bdc7eb7dc8b949141b7d2bc0a05c85255c82d3
AmVirgo <tenchishishou@hotmail.com>
2022-03-13 04:30:59 +0100

    Pause Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu: MonoBehaviour
+{
+    [SerializeField]private GameObject settingsMenu, pauseMenu;
+    private bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1"); 
+    }
+    public void LoadSettings()
+    {
+        settingsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void LeaveSettings()
+    {
+        pauseMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
